1d6c8da3b6d3cd3ac41147689b4b4472a20b3cd1
Sonu445Kum <sonuroy1629@gmail.com>
2025-09-29 17:20:26 +0530

    solved two examples of do-while loops

---
+++ 4_Loops/3_do-while.php
@@ -0,0 +1,17 @@
+<?php
+// Print 1 to 5 numbers
+$i = 1;
+do{
+    echo $i . " ";
+    $i++;
+} while($i <= 5);
+
+
+// print 10-1 number
+$i = 10;
+do{
+    echo $i . " ";
+    $i--;
+} while($i >= 1);
+
+?>
