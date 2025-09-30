d15b0e348eb02f5540a7e9baa98af5cf469707bd
Sonu445Kum <sonuroy1629@gmail.com>
2025-09-30 15:34:14 +0530

    Solved some Questions of casting as castToNull ,castTOObject,castToArray

---
+++ 9_Casting/castToNull.php
@@ -0,0 +1,20 @@
+<?php
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+// Correct way instead of (unset)
+$a = null;
+$b = null;
+$c = null;
+$d = null;
+$e = null;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+?>
---
+++ 9_Casting/castToObjects.php
@@ -0,0 +1,20 @@
+<?php
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (object) $a;
+$b = (object) $b;
+$c = (object) $c;
+$d = (object) $d;
+$e = (object) $e;
+
+//To verify the type of any object in PHP, use the var_dump() function:
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
+?> 
