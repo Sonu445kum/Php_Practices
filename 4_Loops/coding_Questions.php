bb53275338b47e2c277d169811ef195dde9ec040
Sonu445Kum <sonuroy1629@gmail.com>
2025-10-01 12:46:08 +0530

    Solved 10  Coding Problems of Loops

---
+++ 4_Loops/coding_Questions.php
@@ -0,0 +1,76 @@
+<?php
+// for loop ka use karke 1 se 10 tak numbers print karo.
+// for($i=1; $i<=10; $i++){
+//     echo $i." "; 
+// }
+
+// while loop ka use karke 1 se 5 tak numbers print karo.
+// while($i<=10){
+//     echo $i." ";
+//     $i++;
+// }
+
+// do...while loop ka use karke 1 se 5 tak numbers print karo.
+// Initialize variable
+// $i = 1;
+
+// do {
+//     echo $i . " "; // Current number print karo
+//     $i++;           // Variable ko increment karo
+// } while ($i <= 5); // Condition check karo
+
+// foreach loop ka use karke ek array ke elements print karo.
+// $num = array(1,2,3,4,5,6,7);
+// foreach($num as $n){
+//     echo $n." ";
+// }
+
+// Ek loop banao jo 1 se 100 tak ke even numbers print kare.
+// for($i=1; $i<=100; $i++){
+//     if($i % 2==0){
+//         echo $i." ";
+//     }
+// }
+
+
+// Ek loop banao jo 1 se 100 tak ke odd numbers print kare.
+// for($i=1; $i<=100; $i++){
+//     if($i % 2!=0){
+//         echo $i." ";
+//     }
+// }
+
+// Ek program likho jo factorial nikaale kisi number ka using loop.
+// $num=5;
+// $prod=1;
+// for($i = $num; $i>=1; $i--){  
+// $prod*=$i;  
+// }
+// echo "The Factorial Of Number:".$prod;
+
+// Ek nested loop likho jo multiplication table print kare (1–10).
+// for($i = 1; $i <= 10; $i++) {
+//     echo "Multiplication table of $i:<br>";
+//     for($j = 1; $j <= 10; $j++) {
+//         $result = $i * $j;
+//         echo "$i × $j = $result<br>";
+//     }
+//     echo "<br>"; // Next table ke liye line break
+// }
+
+// Ek loop likho jo array me se sabse bada number find kare.
+// $num = array(1,2,3,4,5,6);
+// $max =$num[0];
+// for($i=0; $i<count($num); $i++){
+//     if($num[$i] >$max){
+//         $max =$num[$i];
+//     }
+// };
+// echo "The Max Number in the array:".$max."\n";
+
+// Ek loop likho jo ek string ke characters ko alag-alag line me print kare.
+// $string = "Hello, World!";
+// for($i = 0; $i < strlen($string); $i++) {
+//     echo $string[$i] . " ";
+// }
+?>
